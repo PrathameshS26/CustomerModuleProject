12fbfa46947eeac17e290ea39a8ddd70b01a66f4
Akash Patil <patilakash229@gmail.com>
2021-06-01 11:58:05 +0530

    Final Final updated Customer

--- src/main/java/com/cg/fleetmanagement/customermodule/controller/CustomerUnitController.java
+++ src/main/java/com/cg/fleetmanagement/customermodule/controller/CustomerUnitController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -35,6 +36,7 @@ import com.cg.fleetmanagement.customermodule.repository.CustomerRepository;
 import com.cg.fleetmanagement.customermodule.serviceimplementation.CustomerUnitImplementation;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/customers")
 public class CustomerUnitController {
 
